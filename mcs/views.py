aad9089ae278fcc1607dfe9aabdb2fc0513a3a49
gh 6141 <gh6141@gmail.com>
2022-05-21 12:57:43 +0900

    up

---
+++ mcs/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+def testfunc(request):
+    return HttpResponse('test')
+
+class Hclass(TemplateView):
+    template_name='test2.html'
